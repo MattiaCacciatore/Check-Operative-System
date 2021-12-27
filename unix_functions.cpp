b72051bfcc88b62a7c04c3950c6aaa542a566553
Mattia Cacciatore <73939270+MattiaCacciatore@users.noreply.github.com>
2021-12-27 03:49:43 +0100

    Final version.

--- unix_functions.cpp
+++ unix_functions.cpp
@@ -3,16 +3,17 @@
 
 int unix::my_printf(void){
     const int fd_file_out = open("test.txt", O_WRONLY);
+    // Open or create a text file named "test".
     if(fd_file_out < 0){
 	perror("Open failed");
 	return -1;
     }
-
+    // Redirect the standard output on file.
     if(dup2(fd_file_out, STDOUT_FILENO) < 0){
 	perror("Dup2 failed");
 	return -1;
     }
-
+    // Close the file descriptor.
     if(close(fd_file_out) < 0){
 	perror("Close failed");
 	return -1;
