8f6d0af1dabb64598204febfd6dada9b6ddb7fc3
Mattia Cacciatore <73939270+MattiaCacciatore@users.noreply.github.com>
2021-12-27 03:46:55 +0100

    Update.

--- unix_functions.cpp
+++ unix_functions.cpp
@@ -1,27 +1,23 @@
-/* Unix functions. */
+// Unix functions.
 #include "unix_os.h"
 
-const int unix::my_printf(void)
-{
+int unix::my_printf(void){
     const int fd_file_out = open("test.txt", O_WRONLY);
-    if(fd_file_out < 0)
-	{
-       perror("Open failed");
-	   return -1;
-	}
+    if(fd_file_out < 0){
+	perror("Open failed");
+	return -1;
+    }
 
-    if(dup2(fd_file_out, STDOUT_FILENO) < 0)
-	{
-       perror("Dup2 failed");
-	   return -1;
-	}
+    if(dup2(fd_file_out, STDOUT_FILENO) < 0){
+	perror("Dup2 failed");
+	return -1;
+    }
 
-    if(close(fd_file_out) < 0)
-	{
-       perror("Close failed");
-	   return -1;
-	}
+    if(close(fd_file_out) < 0){
+	perror("Close failed");
+	return -1;
+    }
 	
-	printf("OS UNIX DETECTED.\n");
-	return 0;
+   printf("OS UNIX DETECTED.\n"); // On stdout --> test.txt
+   return 0;
 }
